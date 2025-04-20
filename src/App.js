5cebb86437f68952f3309cecfb9f18985bc77f8c
laxmanrao23 <laxmanrao.savirigapu@gmail.com>
2025-04-20 18:26:06 +0530

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Home from './sections/Home';
 import Contact from './sections/Contact';
